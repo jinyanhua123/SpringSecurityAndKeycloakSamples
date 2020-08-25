9685b5fc9824b5fa76cd1d7de9f923dc74a45260
yanhua.jin <yanhua.jin@g.softbank.co.jp>
2020-08-25 09:44:45 +0900

    edit

--- resource_server_demo/src/main/java/com/example/demo/SecurityConfig.java
+++ resource_server_demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,8 +17,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/hello").hasAuthority("SCOPE_hello")
                 .antMatchers(HttpMethod.GET, "/list").hasAuthority("SCOPE_user")
                 .antMatchers(HttpMethod.GET, "/").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .cors()
+                .configurationSource(this.corsConfigurationSource());
         http.oauth2ResourceServer()
                 .jwt();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedMethod("GET");
+        corsConfiguration.addAllowedOrigin("http://localhost:9020");
+        corsConfiguration.addAllowedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/", corsConfiguration);
+        corsSource.registerCorsConfiguration("/list", corsConfiguration);
+        corsSource.registerCorsConfiguration("/hello", corsConfiguration);
+        return corsSource;
     }
 }
